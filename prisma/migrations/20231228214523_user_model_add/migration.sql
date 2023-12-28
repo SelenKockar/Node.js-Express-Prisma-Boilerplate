a369290cf4470673a412a5215cf0952d586bc14e
SelenKockar <selen.kockar@tedu.edu.tr>
2023-12-29 00:47:55 +0300

    Change Prisma schema

--- prisma/migrations/20231228214523_user_model_add/migration.sql
+++ prisma/migrations/20231228214523_user_model_add/migration.sql
@@ -3,7 +3,7 @@ CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "username" TEXT NOT NULL,
     "email" TEXT NOT NULL,
-    "passwordHash" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
